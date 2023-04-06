c97ec60610ab1cdbe70ea3c1c45c73118910b635
itstaami <eegii0977@gmail.com>
2023-04-07 02:52:51 +0800

    Apr7

--- controller/moviesController.ts
+++ controller/moviesController.ts
@@ -2,11 +2,33 @@ import { Request, Response } from "express";
 
 import Movie from "../model/Movie";
 
+// const getMovies = async (req: Request, res: Response) => {
+//   try {
+//     const movies = await Movie.find({}).limit(10);
+//     res.status(200).json({ message: "All Movies", movies });
+//   } catch (err) {
+//     res.status(400).json({
+//       message: "Movies medeelliig avhad aldaa garlaa",
+//     });
+//   }
+// };
 const getMovies = async (req: Request, res: Response) => {
+  const limit = Number(req.query.limit) || 10;
+  const page = Number(req.query.page) || 1;
+
+  const total = await Movie.countDocuments();
+  const pageCount = Math.ceil(total / limit);
+  const skip = (page - 1) * limit || 0;
+
   try {
-    const movies = await Movie.find({}).limit(10);
-    res.status(200).json({ message: "All Movies", movies });
-  } catch (err) {
+    const movies = await Movie.find().skip(skip).limit(limit);
+    res.status(200).json({
+      message: "Movies",
+      movies,
+      pagination: { total, pageCount, page },
+    });
+  } catch (error) {
+    // console.log("ERROR ", error);
     res.status(400).json({
       message: "Movies medeelliig avhad aldaa garlaa",
     });
@@ -23,8 +45,6 @@ const getMovie = async (req: Request, res: Response) => {
     });
   }
 };
-
-export { getMovies, getMovie };
 
 // const getMovies = async (req: Request, res: Response) => {
 //   const limit = Number(req.query.limit) || 10;
@@ -47,3 +67,5 @@ export { getMovies, getMovie };
 //     });
 //   }
 // };
+
+export { getMovies, getMovie };
